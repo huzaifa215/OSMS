aed50168711abc740940e728e8cf5fdc72b28e19
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2020-09-27 15:15:44 +0500

    conatct from

--- index.php
+++ index.php
@@ -53,6 +53,8 @@
     </div>
   </header> <!-- End Header Jumbotron -->
 
+
+
   <div class="container">
     <!--Introduction Section-->
     <div class="jumbotron">
@@ -78,6 +80,8 @@
     </div>
   </div>
   <!--Introduction Section End-->
+
+
   <!-- Start Services -->
   <div class="container text-center border-bottom" id="Services">
     <h2>Our Services</h2>
@@ -96,6 +100,45 @@
       </div>
     </div>
   </div> <!-- End Services -->
+
+<!-- start registertion form -->
+<div class="container pt-5">
+    <h2 class="text-center">Create an Account</h2>
+    <div class="row mt-4 mb-4">
+<div class="col-md-6 offset-md-3">
+
+<form action="" class="shadow-lg p-4" method="POST">
+
+<div class="from-group">
+    <i class="fas fa-user"></i>
+    <label for="name" class="font-weight-bold p1-2">Name</label>
+    <input type="text" class="form-control" placeholder="Enter your Name" name="rName">
+
+</div>
+
+<div class="from-group">
+    <i class="fas fa-user"></i>
+    <label for="email" class="font-weight-bold p1-2">Email</label>
+    <input type="email" class="form-control" placeholder="Enter your email" name="rEmail">
+<small class="form-text">We'll never share your email</small>
+</div>
+
+<div class="from-group">
+    <i class="fas fa-key"></i>
+    <label for="pass" class="font-weight-bold p1-2">New Password</label>
+    <input type="password" class="form-control" placeholder="Enter your password" name="rPassword">
+
+</div>
+ <button type="submit" class="btn btn-danger mt-5 btn-block shadow-sm font-weight-bold" name="rSignup">Sign Up</button>
+ <em style="font-size:10px;">Note: By clicking sign up you agree our term and conditions</em>
+</form>
+</div>
+
+    </div>
+</div>
+<!-- end start registertaion form-->
+
+
 
   <!-- Start Registration  -->
   <?php include('userRegistration.php') ?>
