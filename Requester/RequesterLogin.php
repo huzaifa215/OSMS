27ed1f542e28b90908dfadeb0979788ed9980375
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2020-09-28 21:39:37 +0500

    login form

---
+++ Requester/RequesterLogin.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Bootstrap CSS -->
+  <!-- ../ tu one level up -->
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+  <!-- Font Awesome CSS -->
+  <link rel="stylesheet" href="../css/all.min.css">
+
+  <!-- <style>
+    .custom-margin {
+         margin-top: 8vh;
+      }
+   </style> -->
+  <title>Login</title>
+</head>
+<body>
+    <center>
+<div class="mb-3 text-center mt-5" style="font-size: 30px;">
+    <i class="fas fa-stethoscope"></i>
+    <span>Online Service Managment System</span>
+  </div>
+    </center>
+
+    
+  <!-- Boostrap JavaScript -->
+  <script src="../js/jquery.min.js"></script>
+  <script src="../js/popper.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/all.min.js"></script>
+  
+</body>
+
+</html>
+  
