5eacb78432c7f8c5769f129021c7f3c704a37813
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2020-09-28 20:32:30 +0500

    form database

--- UserRegistration.php
+++ UserRegistration.php
@@ -1,3 +1,34 @@
+<?php
+  include('dbConnection.php');
+
+  if(isset($_REQUEST['rSignup'])){
+    // Checking for Empty Fields
+    if(($_REQUEST['rName'] == "") || ($_REQUEST['rEmail'] == "") || ($_REQUEST['rPassword'] == "")){
+     $regmsg = '<div class="alert alert-danger mt-2" role="alert"> All Fields are Required </div>';
+    // $regmsg="All feilds are requires";
+    } else {
+    //   $sql = "SELECT r_email FROM requesterlogin_tb WHERE r_email='".$_REQUEST['rEmail']."'";
+    //   $result = $conn->query($sql);
+    //   if($result->num_rows == 1){
+    //     $regmsg = '<div class="alert alert-warning mt-2" role="alert"> Email ID Already Registered </div>';
+    //   } else {
+    //     // Assigning User Values to Variable
+        $rName = $_REQUEST['rName'];
+        $rEmail = $_REQUEST['rEmail'];
+        $rPassword = $_REQUEST['rPassword'];
+        $sql = "INSERT INTO requesterlogin_tb(r_name, r_email, r_password) VALUES ('$rName','$rEmail', '$rPassword')";
+        if($conn->query($sql) == TRUE){
+          $regmsg = '<div class="alert alert-success mt-2" role="alert"> Account Succefully Created </div>';
+        } else {
+          $regmsg = '<div class="alert alert-danger mt-2" role="alert"> Unable to Create Account </div>';
+        }
+      }
+    }
+ // }
+?>
+
+
+
 <!-- start registertion form -->
 <div class="container pt-5" id="registration">
     <h2 class="text-center">Create an Account</h2>
@@ -28,6 +59,10 @@
 </div>
  <button type="submit" class="btn btn-danger mt-5 btn-block shadow-sm font-weight-bold" name="rSignup">Sign Up</button>
  <em style="font-size:10px;">Note: By clicking sign up you agree our term and conditions</em>
+ <?php if(isset($regmsg)){
+     echo $regmsg;
+ }
+ ?>
 </form>
 </div>
 
--- dbConnection.php
+++ dbConnection.php
@@ -8,10 +8,7 @@ $db_port=3306;
 $con=new mysqli($db_host,$db_user,$db_password,$db_name,$db_port);
 //chechking connection
 if($con->connect_errno){
-    echo"connection failed";
-}
-else{
-    echo" Connectd";
+    die("connection failed");
 }
 
 ?>
