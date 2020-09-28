418a52cff2c5af07827a58e5ae5ee3f0a9600848
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2020-09-28 22:07:19 +0500

    bosststrap link and css links

--- Requester/RequesterLogin.php
+++ Requester/RequesterLogin.php
@@ -1,41 +1,90 @@
+<?php
+include('../dbConnection.php');
+session_start();
+if(!isset($_SESSION['is_login'])){
+  if(isset($_REQUEST['rEmail'])){
+    $rEmail = mysqli_real_escape_string($conn,trim($_REQUEST['rEmail']));
+    $rPassword = mysqli_real_escape_string($conn,trim($_REQUEST['rPassword']));
+    $sql = "SELECT r_email, r_password FROM requesterlogin_tb WHERE r_email='".$rEmail."' AND r_password='".$rPassword."' limit 1";
+    $result = $conn->query($sql);
+    if($result->num_rows == 1){
+      
+      $_SESSION['is_login'] = true;
+      $_SESSION['rEmail'] = $rEmail;
+      // Redirecting to RequesterProfile page on Correct Email and Pass
+      echo "<script> location.href='RequesterProfile.php'; </script>";
+      exit;
+    } else {
+      $msg = '<div class="alert alert-warning mt-2" role="alert"> Enter Valid Email and Password </div>';
+    }
+  }
+} else {
+  echo "<script> location.href='RequesterProfile.php'; </script>";
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
   <!-- Bootstrap CSS -->
-  <!-- ../ tu one level up -->
   <link rel="stylesheet" href="../css/bootstrap.min.css">
 
   <!-- Font Awesome CSS -->
   <link rel="stylesheet" href="../css/all.min.css">
 
-  <!-- <style>
+  <style>
     .custom-margin {
          margin-top: 8vh;
       }
-   </style> -->
+   </style>
   <title>Login</title>
 </head>
-<body>
-    <center>
-<div class="mb-3 text-center mt-5" style="font-size: 30px;">
-    <i class="fas fa-stethoscope"></i>
-    <span>Online Service Managment System</span>
-  </div>
-    </center>
 
-    
+<body>
+  <div class="mb-3 text-center mt-5" style="font-size: 30px;">
+    <i class="fas fa-stethoscope"></i>
+    <span>Online Maintenance Managment System</span>
+  </div>
+  <p class="text-center" style="font-size: 20px;"> <i class="fas fa-user-secret text-danger"></i> <span>Requester
+      Area(Demo)</span>
+  </p>
+  <div class="container-fluid mb-5">
+    <div class="row justify-content-center custom-margin">
+      <div class="col-sm-6 col-md-4">
+        <form action="" class="shadow-lg p-4" method="POST">
+          <div class="form-group">
+            <i class="fas fa-user"></i><label for="email" class="pl-2 font-weight-bold">Email</label><input type="email"
+              class="form-control" placeholder="Email" name="rEmail">
+            <!--Add text-white below if want text color white--><br>
+            <small class="form-text">We'll never share your email with anyone else.</small>
+          </div>
+          <div class="form-group">
+            <i class="fas fa-key"></i><label for="pass" class="pl-2 font-weight-bold">Password</label><input type="password"
+              class="form-control" placeholder="Password" name="rPassword">
+          </div>
+          <button type="submit" class="btn btn-outline-danger mt-3 btn-block shadow-sm font-weight-bold">Login</button>
+          <?php if(isset($msg)) {echo $msg; } ?>
+        </form>
+        <div class="text-center"><a class="btn btn-info mt-3 shadow-sm font-weight-bold" href="../index.php">Back
+            to Home</a></div>
+      </div>
+    </div>
+  </div>
+
   <!-- Boostrap JavaScript -->
   <script src="../js/jquery.min.js"></script>
   <script src="../js/popper.min.js"></script>
   <script src="../js/bootstrap.min.js"></script>
   <script src="../js/all.min.js"></script>
-  
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
 
 </html>
-  
