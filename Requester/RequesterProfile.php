ca719af71836a8a3cf0248ae14fe86a3d9e2ac8f
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2020-09-30 22:42:15 +0500

    navbar

--- Requester/RequesterProfile.php
+++ Requester/RequesterProfile.php
@@ -49,10 +49,37 @@
   <link rel="stylesheet" href="../css/all.min.css">
 
   
-  <title>Login</title>
+  <title>Profile</title>
 </head>
 
 <body>
+  <!-- navbar -->
+<nav class="navbar navbar-light bg-danger shadow">
+  <span class="navbar-brand mb-0 h1">
+  <a class="navbar-brand" href="RequesterProfile.php">
+   OSMS
+  </a>
+  </span>
+</nav>
+<!-- side bar -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <div class="col-sm-6 mt-5">
   <form class="mx-5" method="POST">
     <div class="form-group">
@@ -72,5 +99,8 @@
 <?php
 include('includes/footer.php'); 
 ?>
+ <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
 </body>
 </html>
