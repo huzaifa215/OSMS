9c716d5fc6930111b20ef4b4a6eaac4280c81362
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2020-09-30 23:09:03 +0500

    Requester profile

--- Requester/RequesterProfile.php
+++ Requester/RequesterProfile.php
@@ -38,6 +38,7 @@
 
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -65,7 +66,7 @@
 
 <div class="container-fluid">
   <div class="row">
-    <div class="col-sm-2 bg-light sidebar">
+    <div class="col-sm-2 bg-light sidebar py-5">
       <div><ul class="nav flex-colum"><li class="nac-item"><a class="nav-link href="RequesterProfile.php"><i class="fas fa-user"></i>Profile</a></li>
 
       </ul>
