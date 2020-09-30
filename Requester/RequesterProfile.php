883707479edcf95efcb36cdd5f85a0181a5c7eff
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2020-09-30 22:53:43 +0500

    side bar

--- Requester/RequesterProfile.php
+++ Requester/RequesterProfile.php
@@ -63,21 +63,17 @@
 </nav>
 <!-- side bar -->
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<div class="container">
+  <div class="row">
+    <div class="col-sm-8">col-sm-8</div>
+    <div class="col-sm-4">col-sm-4</div>
+  </div>
+  <div class="row">
+    <div class="col-sm">col-sm</div>
+    <div class="col-sm">col-sm</div>
+    <div class="col-sm">col-sm</div>
+  </div>
+</div>
 
 
 <div class="col-sm-6 mt-5">
