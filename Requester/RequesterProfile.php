bfc1fc938378faf4956c8cc36af913c4fd96c37d
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2020-10-02 21:54:00 +0500

    profile form

--- Requester/RequesterProfile.php
+++ Requester/RequesterProfile.php
@@ -101,16 +101,8 @@
               
       </ul>
     </div>
-</nav>
-  </div>
-  </div>
-  <!-- side bar end -->
-<!-- java sript -->
-<script src="../js/jquery.min.js"></script>
-  <script src="../js/popper.min.js"></script>
-  <script src="../js/bootstrap.min.js"></script>
-  <script src="../js/all.min.js"></script>
-
+</nav><!-- !st colum end -->
+<!-- form for 2nd colum -->
 <div class="col-sm-6 mt-5">
   <form class="mx-5" method="POST">
     <div class="form-group">
@@ -124,12 +116,20 @@
     <button type="submit" class="btn btn-danger" name="nameupdate">Update</button>
     <?php if(isset($passmsg)) {echo $passmsg; } ?>
   </form>
+
+  </div>
+  </div>
 </div>
-</div>
-</div>
+  <!-- side bar end -->
+
+
  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-
+<!-- java sript -->
+<script src="../js/jquery.min.js"></script>
+  <script src="../js/popper.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/all.min.js"></script>
 <?php
 include('includes/footer.php'); 
 ?>
