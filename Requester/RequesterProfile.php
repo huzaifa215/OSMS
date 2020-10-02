93d08659cfa6ffd6fd069aac3b9df5f622577692
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2020-10-02 21:18:09 +0500

    sidebar

--- Requester/RequesterProfile.php
+++ Requester/RequesterProfile.php
@@ -38,47 +38,78 @@
 
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="./online-service-management-system-in-php/css/bootstrap.min.css">
 
   <!-- Font Awesome CSS -->
-  <link rel="stylesheet" href="../css/all.min.css">
-
-  
+  <link rel="stylesheet" href="./online-service-management-system-in-php/css/all.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <title>Profile</title>
 </head>
 
 <body>
   <!-- navbar -->
-<nav class="navbar navbar-light bg-danger shadow">
+<nav class="navbar navbar-light fixed-top  bg-danger flex-md-nowrap p-0 shadow">
   <span class="navbar-brand mb-0 h1">
-  <a class="navbar-brand" href="RequesterProfile.php">
+  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="RequesterProfile.php">
    OSMS
   </a>
-  </span>
+
 </nav>
 <!-- side bar -->
-
-<div class="container-fluid">
+<div class="container-fluid" style="margin-top:40px;">
   <div class="row">
-    <div class="col-sm-2 bg-light sidebar py-5">
-      <div><ul class="nav flex-colum"><li class="nac-item"><a class="nav-link href="RequesterProfile.php"><i class="fas fa-user"></i>Profile</a></li>
+    <nav class="col-sm-2 bg-light sidebar py-5">
+      
+    <!-- <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-app-indicator" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M5.5 2A3.5 3.5 0 0 0 2 5.5v5A3.5 3.5 0 0 0 5.5 14h5a3.5 3.5 0 0 0 3.5-3.5V8a.5.5 0 0 1 1 0v2.5a4.5 4.5 0 0 1-4.5 4.5h-5A4.5 4.5 0 0 1 1 10.5v-5A4.5 4.5 0 0 1 5.5 1H8a.5.5 0 0 1 0 1H5.5z"/>
+  <path d="M16 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+</svg> -->
 
+      <div class="sidebar-sticky">
+        <ul class="nav flex-colum">
+
+          <li class="nav-item">
+            <a class="nav-link" href="RequesterProfile.php">
+              <i class="fas fa-user"></i>Profile</a></li>
+
+              <li class="nav-item">
+            <a class="nav-link" >
+              <i class="fas fa-user"></i></a></li>
+
+              <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="fas fa-user"></i>Submit request</a></li>
+
+              <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="fas fa-user"></i>Service Status</a></li>
+
+              <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="fas fa-user"></i>Change Password</a></li>
+
+              <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="fas fa-sign-out-alt"></i>LogOut</a></li>
+
+              
       </ul>
-    
     </div>
-    </div>
+</nav>
   </div>
-
-  
   </div>
-
-
+  <!-- side bar end -->
+<!-- java sript -->
+<script src="../js/jquery.min.js"></script>
+  <script src="../js/popper.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/all.min.js"></script>
 
 <div class="col-sm-6 mt-5">
   <form class="mx-5" method="POST">
@@ -96,11 +127,12 @@
 </div>
 </div>
 </div>
+ <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
 <?php
 include('includes/footer.php'); 
 ?>
- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
 </body>
 </html>
