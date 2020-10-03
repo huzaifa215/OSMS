00157b55f1cb05a5929777f9810aa4c169d17f07
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2020-10-03 17:57:24 +0500

    Footer header files

---
+++ Requester/SubmitRequest.php
@@ -0,0 +1,79 @@
+<?php  include('include/header.php')?>
+
+<div class="col-sm-9 col-md-10 mt-5">
+  <form class="mx-5" action="" method="POST">
+    <div class="form-group">
+      <label for="inputRequestInfo">Request Info</label>
+      <input type="text" class="form-control" id="inputRequestInfo" placeholder="Request Info" name="requestinfo">
+    </div>
+    <div class="form-group">
+      <label for="inputRequestDescription">Description</label>
+      <input type="text" class="form-control" id="inputRequestDescription" placeholder="Write Description" name="requestdesc">
+    </div>
+    <div class="form-group">
+      <label for="inputName">Name</label>
+      <input type="text" class="form-control" id="inputName" placeholder="Rahul" name="requestername">
+    </div>
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="inputAddress">Address Line 1</label>
+        <input type="text" class="form-control" id="inputAddress" placeholder="House No. 123" name="requesteradd1">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputAddress2">Address Line 2</label>
+        <input type="text" class="form-control" id="inputAddress2" placeholder="Railway Colony" name="requesteradd2">
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="inputCity">City</label>
+        <input type="text" class="form-control" id="inputCity" name="requestercity">
+      </div>
+      <div class="form-group col-md-4">
+        <label for="inputState">State</label>
+        <input type="text" class="form-control" id="inputState" name="requesterstate">
+      </div>
+      <div class="form-group col-md-2">
+        <label for="inputZip">Zip</label>
+        <input type="text" class="form-control" id="inputZip" name="requesterzip" onkeypress="isInputNumber(event)">
+      </div>
+    </div>
+
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="inputEmail">Email</label>
+        <input type="email" class="form-control" id="inputEmail" name="requesteremail">
+      </div>
+      <div class="form-group col-md-2">
+        <label for="inputMobile">Mobile</label>
+        <input type="text" class="form-control" id="inputMobile" name="requestermobile" onkeypress="isInputNumber(event)">
+      </div>
+      <div class="form-group col-md-2">
+        <label for="inputDate">Date</label>
+        <input type="date" class="form-control" id="inputDate" name="requestdate">
+      </div>
+    </div>
+
+    <button type="submit" class="btn btn-danger" name="submitrequest">Submit</button>
+    <button type="reset" class="btn btn-secondary">Reset</button>
+  </form>
+  <!-- below msg display if required fill missing or form submitted success or failed -->
+  <?php if(isset($msg)) {echo $msg; } ?>
+</div>
+</div>
+</div>
+<!-- Only Number for input fields -->
+<script>
+  function isInputNumber(evt) {
+    var ch = String.fromCharCode(evt.which);
+    if (!(/[0-9]/.test(ch))) {
+      evt.preventDefault();
+    }
+  }
+</script>
+<?php
+include('include/footer.php'); 
+// $conn->close();
+?>
+<br>
+
